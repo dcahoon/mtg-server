f0a1f6cbc128fd343ada57b5f540f5885441a974
Dylan Cahoon <dylancahoon651@gmail.com>
2022-01-15 14:54:07 -0500

    new

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 
 app.use(express.json())
 
-app.use(cors({ origin: "localhost:3000"}))
+app.use(cors({ origin: "http://localhost:3000"}))
 app.use("/decks", decksRouter)
 
 // Not found handler
