52598a49ef5c26b6e80996e691d0dee97c180ef7
Dylan Cahoon <dylancahoon651@gmail.com>
2022-01-20 17:38:26 -0500

    new commit

--- src/db/fixtures/decks.js
+++ src/db/fixtures/decks.js
@@ -1,13 +1,44 @@
 module.exports = [
     {
-        user_id: "0001",
+        user_id: "1",
         deck_name: "Miracle Lapse",
-        deck_cards: [],
+        deck_cards: [
+            {
+                name: "Memory Lapse",
+                cmc: 2,
+                quantity: 4,
+            },
+            {
+                name: "Brainstorm",
+                cmc: 1,
+                quantity: 4,
+            },
+            {
+                name: "Entreat the Angels",
+                cmc: 3,
+                quantity: 4,
+            },
+            {
+                name: "Temporal Mastery",
+                cmc: 7,
+                quantity: 4,
+            },
+            {
+                name: "Isochron Scepter",
+                cmc: 2,
+                quantity: 4,
+            },
+            {
+                name: "Azorius Charm",
+                cmc: 2,
+                quantity: 4,
+            }
+        ],
     },
     {
-        user_id: "0001",
+        user_id: "1",
         deck_name: "Forbidden Illness",
-        deck_cards: [],
+        deck_cards: "",
     }
 ]
 
--- src/db/fixtures/users.js
+++ src/db/fixtures/users.js
@@ -1,7 +1,37 @@
 module.exports = [
     {
-        username: "Dylan",
-        location: "USA",
-        user_id: "0001"
+        "user_id": 1,
+        "username": "Dylan",
+        "location": "USA",
+        "created_at": "2022-01-18T22:47:58.181Z",
+        "updated_at": "2022-01-18T22:47:58.181Z"
+    },
+    {
+        "user_id": 2,
+        "username": "Cardy B",
+        "location": "Canada",
+        "created_at": "2022-01-19T14:55:57.434Z",
+        "updated_at": "2022-01-19T14:55:57.434Z"
+    },
+    {
+        "user_id": 3,
+        "username": "Cardy A",
+        "location": "Mexico",
+        "created_at": "2022-01-20T01:09:09.330Z",
+        "updated_at": "2022-01-20T01:09:09.330Z"
+    },
+    {
+        "user_id": 4,
+        "username": "Cardy C",
+        "location": "Mexico",
+        "created_at": "2022-01-20T01:09:26.926Z",
+        "updated_at": "2022-01-20T01:09:26.926Z"
+    },
+    {
+        "user_id": 5,
+        "username": "YuGiOh",
+        "location": "Mexico",
+        "created_at": "2022-01-20T01:17:02.460Z",
+        "updated_at": "2022-01-20T01:17:02.460Z"
     }
 ]
